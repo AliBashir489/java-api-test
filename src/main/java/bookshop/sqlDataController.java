f15181b2bf826495697e29dfb4a1ce23ff171264
AliBashir489 <94872076+AliBashir489@users.noreply.github.com>
2022-07-22 20:04:55 +0500

    Update sqlDataController.java
    Update file with changes

--- src/main/java/bookshop/sqlDataController.java
+++ src/main/java/bookshop/sqlDataController.java
@@ -2,21 +2,19 @@ package bookshop;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-public class sqlDataController {
+public class BookRetrievalController {
 	
-	private String string = "";
+	public String string = "";
 	
 	@GetMapping("/books/all")
 	@ResponseBody
 	public String allBooks() {
-		string ="All Books:<br>" + new sqlBookData("SELECT * FROM book").returnData;
+		string ="All Books:<br>" + new BookRetrievalFeature("SELECT * FROM book").returnData;
 		return string;
 	}
 	
@@ -25,20 +23,24 @@ public class sqlDataController {
 	@ResponseBody
 	public String booksByGenre(@PathVariable String genre){
 		string = "All " + genre + " Books:<br>"
-				+ new sqlBookData("SELECT * FROM book WHERE genre = \"" + genre + "\"").returnData;
+				+ new BookRetrievalFeature("SELECT * FROM book WHERE genre = \"" + genre + "\"").returnData;
 		return string;
 	}
 	
 	
 	@GetMapping("/books/mostsold")
 	public String booksByGenre(){
-		string ="Top 10 Most Sold Books:<br>"
-				+ new sqlBookData("SELECT * FROM book ORDER BY copies_sold DESC LIMIT 10").returnData;
+		string = "Top 10 Most Sold Books:<br>"
+				+ new BookRetrievalFeature("SELECT * FROM book ORDER BY copies_sold DESC LIMIT 10").returnData;
 		return string;	
 	}
 	
-//	@GetMapping("/books/custom")
-//	public String booksByCustom(@RequestParam(value = "from", defaultValue = "0")String from){
-//		return new sqlBookData("SELECT * FROM BooksData ORDER BY copies_sold LIMIT " + from + ", 23").returnData;
-//	}	
+	@GetMapping("/books/custom/{chunkSize}/{chunkNum}")
+	public String booksByCustom(@PathVariable int chunkSize, @PathVariable int chunkNum){
+		string = "Page " + chunkNum + " - Displaying " + chunkSize + " Entries:<br><br>" +
+				new BookRetrievalFeature("SELECT * FROM book LIMIT "
+				+ ((chunkNum*chunkSize) - chunkSize) + ", " + chunkSize).returnData;
+		
+		return string;
+	}	
 }
